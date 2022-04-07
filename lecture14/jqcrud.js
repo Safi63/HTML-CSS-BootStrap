315ba4628b59af8f824d75c22e4f102116962546
Safi Ullah Muhammad <96912261+Safi63@users.noreply.github.com>
2022-04-07 23:25:29 +0500

    HTML

---
+++ lecture14/jqcrud.js
@@ -0,0 +1,128 @@
+$(function() {
+  loadRecipies();
+  $("#recipes").on("click", ".btn-danger", handleDelete);
+  $("#recipes").on("click", ".btn-warning", handleUpdate);
+  $("#addBtn").click(addRecipe);
+  $("#showBtn").click(showRecipe);
+  $("#updateSave").click(function() {
+    var id = $("#updateId").val();
+    var title = $("#updateTitle").val();
+    var body = $("#updateBody").val();
+    $.ajax({
+      url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+      data: { title, body },
+      method: "PUT",
+      success: function(response) {
+        console.log(response);
+        loadRecipies();
+        $("#updateModal").modal("hide");
+      }
+    });
+  });
+});
+function handleUpdate() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".recipe");
+  let id = parentDiv.attr("data-id");
+  $.get("https://usman-recipes.herokuapp.com/api/recipes/" + id, function(
+    response
+  ) {
+    $("#updateId").val(response._id);
+    $("#updateTitle").val(response.title);
+    $("#updateBody").val(response.body);
+    $("#updateModal").modal("show");
+  });
+}
+function addRecipe() {
+  var title = $("#title").val();
+  var body = $("#body").val();
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes",
+    method: "POST",
+    data: { title, body },
+    success: function(response) {
+      console.log(response);
+      $("#title").val("");
+      $("#body").val("");
+      loadRecipies();
+      $("#addModal").modal("hide");
+      loadRecipies();
+    }
+  });
+}
+
+// function showRecipe() {
+//   var title = $(this);
+//   $.ajax({
+//     url: "https://usman-recipes.herokuapp.com/api/recipes",
+//     method: "GET",
+//     data: { title, body },
+//     success: function(response) {
+//       console.log(response);
+//       $("#title").val("");
+//       $("#body").val("");
+//       loadRecipies();
+//       $("#showModal").modal("hide");
+//       loadRecipies();
+//     }
+//   });
+// }
+
+function handleDelete() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".recipe");
+  let id = parentDiv.attr("data-id");
+  console.log(id);
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+    method: "DELETE",
+    success: function() {
+      loadRecipies();
+    }
+  });
+}
+function loadRecipies() {
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes",
+    method: "GET",
+    error: function(response) {
+      var recipes = $("#recipes");
+      recipes.html("An Error has occured");
+    },
+    success: function(response) {
+      console.log(response);
+      var recipes = $("#recipes");
+      recipes.empty();
+      for (var i = 0; i < response.length; i++) {
+        var rec = response[i];
+        recipes.append(
+          `<div class="recipe" data-id="${rec._id}"><h3>${rec.title}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button> ${rec.body}</p></div>`
+        );
+        // recipes.append("<div><h3>" + rec.title + "</h3></div>");
+      }
+    }
+  });
+}
+
+// function loadRecipies() {
+//   $.ajax({
+//     url: "https://usman-recipes.herokuapp.com/api/recipes",
+//     method: "GET",
+//     error: function(response) {
+//       var recipes = $("#recipes");
+//       recipes.html("An Error has occured");
+//     },
+//     success: function(response) {
+//       console.log(response);
+//       var recipes = $("#recipes");
+//       recipes.empty();
+//       for (var i = 0; i < response.length; i++) {
+//         var rec = response[i];
+//         recipes.append(
+//           `<div class="recipe" data-id="${rec._id}"><h3>${rec.title}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button> ${rec.body}</p></div>`
+//         );
+//         // recipes.append("<div><h3>" + rec.title + "</h3></div>");
+//       }
+//     }
+//   });
+// }
